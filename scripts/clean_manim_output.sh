5d0479cc3893dabeb59828b352c3e83912060ad8
n.moroshkin <seed.suboty@gmail.com>
2026-08-07 12:37:23 +0300

    fix clean_manim_output script

--- scripts/clean_manim_output.sh
+++ scripts/clean_manim_output.sh
@@ -4,17 +4,23 @@ set -e
 
 MEDIA_DIR=${1:-videos/rendered}
 SCENE_NAME=${2}
+FILE_NAME=${3}
 
 echo "Cleaning Manim output..."
 
-rm -rf "$MEDIA_DIR/images"
-rm -rf "$MEDIA_DIR/texts"
+if [ -f "./media/videos/${FILE_NAME}/1080p60/${SCENE_NAME}.mp4" ]; then
 
-if [ -f "$MEDIA_DIR/videos/episode/1920p60/$SCENE_NAME.mp4" ]; then
-    mv "$MEDIA_DIR/videos/episode/1920p60/$SCENE_NAME.mp4" \
-       "$MEDIA_DIR/$SCENE_NAME.mp4"
+    mv "./media/videos/${FILE_NAME}/1080p60/${SCENE_NAME}.mp4" \
+       "$MEDIA_DIR/${SCENE_NAME}.mp4"
+    echo "FULL_HD: $MEDIA_DIR/${SCENE_NAME}.mp4"
 fi
 
-rm -rf "$MEDIA_DIR/videos"
+if [ -f "./media/videos/${FILE_NAME}/1920p60/${SCENE_NAME}.mp4" ]; then
+    mv "./media/videos/${FILE_NAME}/1920p60/${SCENE_NAME}.mp4" \
+       "$MEDIA_DIR/${SCENE_NAME}.mp4"
+    echo "SHORTS: $MEDIA_DIR/${SCENE_NAME}.mp4"
+fi
 
-echo "Done: $MEDIA_DIR/$SCENE_NAME.mp4"
+rm -rf "./media"
+
+echo "Done!"
